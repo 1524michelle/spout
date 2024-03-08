1048f4d2a03f7da6ba9500d3f6014fb2acdb99d6
Michelle Huang <1524michelle@berkeley.edu>
2024-03-08 00:41:07 -0800

    bare bones basic pages: need to fix picktopic, pickphrase, timer, timerrunning

--- app/server.js
+++ app/server.js
@@ -1,12 +1,81 @@
-// app/server.js
+require('dotenv').config({path: '../.env'});
+
 const express = require('express');
+const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
+// middleware setup
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
+
+// routes
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.sendFile(__dirname + '/views/landing.html');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/rules', (req, res) => {
+    res.sendFile(__dirname + '/views/rules.html');
+});
+
+app.get('/lookaway', (req, res) => {
+    res.sendFile(__dirname + '/views/lookaway.html');
+});
+
+app.get('/picktopic', (req, res) => {
+    res.sendFile(__dirname + '/views/picktopic.html');
+});
+
+app.post('/picktopic', (req, res) => {
+    // Handle selection of topic card
+    // Example: Update game state, redirect to pick phrase page, etc.
+});
+
+app.get('/pickphrase', (req, res) => {
+    res.sendFile(__dirname + '/views/pickphrase.html');
+});
+
+app.post('/pickphrase', (req, res) => {
+    // Handle selection of phrase card
+    // Example: Update game state, redirect to timer page, etc.
+});
+
+app.get('/timer', (req, res) => {
+    res.sendFile(__dirname + '/views/timer.html');
+});
+
+app.get('/timerrunning', (req, res) => {
+    // Get selected topic from query parameters
+    const selectedTopic = req.query.topic;
+    // Render timer running page HTML with selected topic
+    res.render(__dirname + '/views/timerrunning', { topic: selectedTopic });
+});
+
+app.get('/timerdone', (req, res) => {
+    res.sendFile(__dirname + '/views/timerdone.html');
+});
+
+// clear session and redirect to rules page
+app.get('/playagain', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error destroying session:', err);
+        } else {
+            console.log('Session destroyed successfully.');
+        }
+    });
+    res.redirect('/rules');
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
