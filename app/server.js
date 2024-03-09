2a58a339bfcaf1ad83e159512caaa90ac8b6c6c3
Michelle Huang <1524michelle@berkeley.edu>
2024-03-08 20:57:08 -0800

    import openai

--- app/server.js
+++ app/server.js
@@ -1,9 +1,11 @@
+import OpenAI from "openai";
 require('dotenv').config({path: '../.env'});
 
 const express = require('express');
 const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3000;
+const openai = new OpenAI();
 
 // middleware setup
 
